ef3f80c688571991068b704cdc2c1ae3197ac92e
Skindstad <jakobskindstadskole@gmail.com>
2023-12-04 17:31:03 +0100

    MainViewModel and packages

--- MauiEditor/ViewModel/MainViewModel.cs
+++ MauiEditor/ViewModel/MainViewModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MauiEditor.ViewModel
 {
-    class MainViewModel
+   public partial class MainViewModel: ObservableObject
     {
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        [ObservableProperty]
+        string text;
+
+        [ICommand]
+        void Add()
+        {
+            if (string.IsNullOrEmpty(text)) return;
+            
+            Items.Add(text);
+            Text = string.Empty;
+        }
     }
 }
