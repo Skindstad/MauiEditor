1073737cfe1b5656ba720664708e6c023b52b332
Yaruohri <bendixforsberg@hotmail.com>
2023-12-09 02:50:09 +0100

    Created kommune window

--- MauiEditor/ViewModel/KommuneViewModel.cs
+++ MauiEditor/ViewModel/KommuneViewModel.cs
@@ -1,15 +1,38 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using MauiEditor.Model;
+using MauiEditor.Repository;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MauiEditor.ViewModel
 {
     public partial class KommuneViewModel : ObservableObject
     {
+        private KommuneRepository repository = [];
+
+        public KommuneViewModel()
+        {
+            Info = new ObservableCollection<Kommune>(repository);
+        }
+
+        [ObservableProperty]
+        ObservableCollection<Kommune> info;
+
+
+        [ObservableProperty]
+        string komNr;
+
+        [ObservableProperty]
+        string city;
+
 
         [ICommand]
         async Task GoBack()
@@ -17,6 +40,32 @@ namespace MauiEditor.ViewModel
             await Shell.Current.GoToAsync("..");
         }
 
+        [ICommand]
+        void Clear()
+        {
+            
+        }
 
+        [ICommand]
+        public void Search()
+        {
+            repository.Search(KomNr, City);
+            Info = new ObservableCollection<Kommune>(repository);
+
+            komNr = string.Empty;
+            City = string.Empty;
+        }
+
+        [ICommand]
+        public void Update()
+        {
+            repository.Update(KomNr, City);
+        }
+
+        [ICommand]
+        public void Add()
+        {
+            repository.Add(KomNr, City);
+        }
     }
 }
